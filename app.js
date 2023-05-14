c17c7837bbdb06f0a4c7ebe45c2d1ad92bfb3d80
Ali-Afifi <ali.m.afify2001@gmail.com>
2023-05-15 00:18:11 +0300

    ..

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-/**
- * User Inputs:
- *      d is the distance.
- *      theta is the firing angle.
- *      v0 is the initial velocity.
- *
- * Indicator:
- *      t1 is the time to reach the max height.
- *      h_max is the max height.
- *      t2 is the time to reach the goal.
- *      h_goal is the ball height at goal plane arrival point.
- */
-
-/* ----------------------------------------------------------------- */
-
 /* Engine Setup */
 
 var Engine = Matter.Engine,
@@ -28,31 +13,48 @@ var Engine = Matter.Engine,
 var engine = Engine.create();
 
 // create a renderer
+
 var render = Render.create({
-	element: document.body,
+	element: document.getElementById("simulation"),
 	engine: engine,
 	options: {
+		width: 1200,
 		height: 500,
 		wireframes: false,
-		background: "#808588",
+		background: "#37C6FA",
 	},
 });
 
-var ball = Bodies.circle(30, 420, 20, {
+var ball = Bodies.circle(825, 420, 20, {
 	friction: 0,
 	frictionAir: 0,
 	inverseInertia: 0,
 	render: {
 		fillStyle: "white",
+		sprite: {
+			texture: "./img/ball.png",
+			xScale: 0.24,
+			yScale: 0.24,
+		},
 	},
 });
 
-var ground = Bodies.rectangle(400, 470, 800, 60, {
+var ground = Bodies.rectangle(450, 470, 1500, 60, {
 	isStatic: true,
 	render: {
 		lineWidth: 2,
 		strokeStyle: "white",
-		fillStyle: "black",
+		fillStyle: "#4CB050",
+	},
+});
+
+var goalLineMarker = Bodies.rectangle(1000, 430, 10, 20, {
+	isSensor: true,
+	isStatic: true,
+	render: {
+		strokeStyle: "#000000",
+		fillStyle: "white",
+		lineWidth: 1,
 	},
 });
 
@@ -60,67 +62,65 @@ engine.world.gravity.y = 1;
 // console.log(ball);
 
 /* ---------------------------------------------------------------------- */
-let d = 0;
-let theta = 0;
-var v0 = 0;
-let t1 = 0;
-let h_max = 0;
-let t2 = 0;
-let h_goal = 0;
+
+let d = 25; // d is the distance.
+let theta = 0; // theta is the firing angle.
+var v0 = 0; // v0 is the initial velocity.
+let t1 = 0; //  t1 is the time to reach the max height.
+let h_max = 0; // h_max is the max height.
+let t2 = 0; // t2 is the time to reach the goal.
+let h_goal = 0; // h_goal is the ball height at goal plane arrival point.
 var trail = [];
 
-const distance = document.querySelector("#distance");
-const angle = document.querySelector("#angle");
-const velocity = document.querySelector("#velocity");
-const start = document.querySelector(".start");
-const reset = document.querySelector(".reset");
-const maxH = document.querySelector(".maxH");
-const goalH = document.querySelector(".goalH");
-const log1 = document.querySelector("#log1");
-const log2 = document.querySelector("#log2");
-const log3 = document.querySelector("#log3");
-const item = document.querySelector(".simulation");
-const canvas = document.querySelector("canvas");
-
-item.appendChild(canvas);
+const distanceInput = document.getElementById("distance-input");
+const angleInput = document.getElementById("angle-input");
+const velocityInput = document.getElementById("velocity-input");
+const startBtn = document.getElementById("start-btn");
+const resetBtn = document.getElementById("reset-btn");
+const maxHeight = document.getElementById("max-height");
+const heightFromGoal = document.getElementById("height-from-goal");
+const distanceValue = document.getElementById("distance-value");
+const angleValue = document.getElementById("angle-value");
+const velocityValue = document.getElementById("velocity-value");
+const simulationDiv = document.getElementById("simulation");
 
 /* Distance Slider*/
 
-distance.addEventListener("input", () => {
-	log1.textContent = distance.value;
-	d = distance.value;
+distanceInput.addEventListener("input", () => {
+	d = distanceInput.value;
+	distanceValue.textContent = d;
 
 	// max distance = 100 m but in pixels = 700px
 	// distance in px = 700 * distance / 100
 
-	Body.setPosition(ball, { x: 30 + distance.value * 7, y: 420 });
+	Body.setPosition(ball, { x: 850 - d * 7, y: 420 });
 });
 
 /* Angle Slider*/
 
-angle.addEventListener("input", () => {
-	log2.textContent = angle.value;
-	theta = angle.value;
+angleInput.addEventListener("input", () => {
+	theta = angleInput.value;
+	angleValue.textContent = theta;
 });
 
 /* Velocity Slider*/
 
-velocity.addEventListener("input", () => {
-	log3.textContent = velocity.value;
-	v0 = velocity.value;
+velocityInput.addEventListener("input", () => {
+	v0 = velocityInput.value;
+	velocityValue.textContent = v0;
 });
 
 /* Start Button */
 
-start.addEventListener("click", startAnimation);
+startBtn.addEventListener("click", startAnimation);
 
 /* Reset Button */
 
-reset.addEventListener("click", resetAnimation);
+resetBtn.addEventListener("click", resetAnimation);
 
 /* ----------------------- Rendering the World and Running the Engine -------------------------- */
 
-Composite.add(engine.world, [ball, ground]);
+Composite.add(engine.world, [ball, ground, goalLineMarker]);
 
 // run the renderer
 Render.run(render);
@@ -135,24 +135,26 @@ Runner.run(runner, engine);
 /* Start Function */
 
 function startAnimation() {
-	theta = theta * (Math.PI / 180);
-	t1 = (v0 * Math.sin(theta)) / 9.8;
-	h_max = v0 * Math.sin(theta) * t1 - 0.5 * 9.8 * t1 ** 2;
-	t2 = d / (v0 * Math.cos(theta));
-	h_goal = h_max - 0.5 * 9.8 * (t2 - t1) ** 2;
-	maxH.textContent = h_max.toFixed(2);
-	goalH.textContent = h_goal.toFixed(2);
+	if (v0 != 0) {
+		theta = theta * (Math.PI / 180);
+		t1 = (v0 * Math.sin(theta)) / 9.8;
+		h_max = v0 * Math.sin(theta) * t1 - 0.5 * 9.8 * t1 ** 2;
+		t2 = d / (v0 * Math.cos(theta));
+		h_goal = h_max - 0.5 * 9.8 * (t2 - t1) ** 2;
+		maxHeight.textContent = h_max.toFixed(2);
+		heightFromGoal.textContent = h_goal.toFixed(2);
 
-	let vx, vy;
-	vx = v0 * Math.cos(theta);
-	vy = -v0 * Math.sin(theta);
-	Body.setVelocity(ball, { x: vx, y: vy });
+		let vx, vy;
+		vx = v0 * Math.cos(theta);
+		vy = -v0 * Math.sin(theta);
+		Body.setVelocity(ball, { x: vx, y: vy });
 
-	Events.on(render, "afterRender", addingPath);
+		Events.on(render, "afterRender", addingPath);
 
-	// for checking
-	// console.log(`time: ${t1}, max.Height: ${h_max}`);
-	// console.log(`time: ${t2}, ball.Height: ${h_goal}`);
+		// for checking
+		console.log(`time: ${t1}, max.Height: ${h_max}`);
+		console.log(`time: ${t2}, ball.Height: ${h_goal}`);
+	}
 }
 
 /* Adding Path funciton */
@@ -170,9 +172,10 @@ function addingPath() {
 		var point = trail[i].position,
 			speed = trail[i].speed;
 
-		var hue = 250 + Math.round((1 - Math.min(1, speed / 10)) * 170);
-		render.context.fillStyle = "hsl(" + hue + ", 100%, 55%)";
-		render.context.fillRect(point.x, point.y, 2, 2);
+		// var hue = 250 + Math.round((1 - Math.min(1, speed / 10)) * 170);
+		// render.context.fillStyle = "hsl(" + hue + ", 100%, 55%)";
+		render.context.fillStyle = "#5A5A5A";
+		render.context.fillRect(point.x, point.y, 4, 4);
 	}
 
 	render.context.globalAlpha = 1;
@@ -185,18 +188,19 @@ function addingPath() {
 
 /* Reset Function */
 function resetAnimation() {
-	velocity.value = 0;
-	distance.value = 0;
-	angle.value = 0;
-	log1.textContent = distance.value;
-	log2.textContent = angle.value;
-	log3.textContent = velocity.value;
-	maxH.textContent = 0;
-	goalH.textContent = 0;
-	v0 = velocity.value;
-	theta = angle.value;
-	d = distance.value;
-	Body.setPosition(ball, { x: 30, y: 420 });
+	velocityInput.value = 0;
+	distanceInput.value = 25;
+	angleInput.value = 0;
+	angleValue.textContent = 0;
+	velocityValue.textContent = 0;
+	distanceValue.textContent = 25;
+	maxHeight.textContent = " 0";
+	heightFromGoal.textContent = " 0";
+	v0 = 0;
+	theta = 0;
+	d = 0;
+	trail = [];
+	Body.setPosition(ball, { x: 825, y: 420 });
 	Body.setVelocity(ball, { x: v0, y: v0 });
 	Events.off(render, "afterRender", addingPath);
 }
